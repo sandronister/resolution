8794b1a4eb53f6fd84bbf877ddc7c86d074559d7
Julio Sandroni <julio.sandroni@gobrax.com>
2024-09-12 09:48:34 -0300

    feat:functions

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,20 @@
-use std::io;
 
-fn convert_to_int(data_input:& String) -> i32{
-    let x = data_input.trim().parse::<i32>().unwrap();
-     x
+fn double(x: i32) -> i32 {
+    x * 2
 }
 
-fn gdc(a: i32, b: i32) -> i32 {
-    if b == 0 {
-        return a;
+fn major(x: i32, y: i32) -> i32 {
+    if x > y {
+        x
     } else {
-        return gdc(b, a % b);
+        y
     }
 }
 
 fn main() {
-   let mut first_input = String::new();
-    let mut second_input = String::new();
+   println!("O Dobro de 32 é {}", double(32));
 
-    println!("Enter the first number: ");
-    io::stdin().read_line(&mut first_input).expect("Failed to read line");
-
-    println!("Enter the second number: ");
-    io::stdin().read_line(&mut second_input).expect("Failed to read line");
-
-    let num1 = convert_to_int(&first_input);
-    let num2 = convert_to_int(&second_input);
-
-    let result = gdc(num1, num2);
-
-    println!("The greatest common divisor of {} and {} is {}", num1, num2, result);
+   println!("O maior entre 32 e 64 é {}", major(32, 64));
 
    
 }
