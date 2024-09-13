c5cc5a863e0964e70f223660a0dd7c6f95594d0b
Julio Sandroni <julio.sandroni@gobrax.com>
2024-09-13 09:26:36 -0300

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,9 @@
 
-fn double(x: i32) -> i32 {
-    x * 2
-}
 
-fn major(x: i32, y: i32) -> i32 {
-    if x > y {
-        x
-    } else {
-        y
-    }
-}
+
 
 fn main() {
-   println!("O Dobro de 32 é {}", double(32));
-
-   println!("O maior entre 32 e 64 é {}", major(32, 64));
+   let input_str = String.New()
 
    
 }
