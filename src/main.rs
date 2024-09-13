d86f07fa61fa081598125f73c308f9f175fd050b
Julio Sandroni <julio.sandroni@gobrax.com>
2024-09-13 09:32:53 -0300

    feat:fatorial with for

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+use std::io;
 
 
+fn convet_to_int(data_input: &String) -> i32 {
+    let x = data_input.trim().parse::<i32>().unwrap();
+    x
+}
 
 
 fn main() {
-   let input_str = String.New()
+   let mut input_str = String::new();
+    io::stdin().read_line(&input_str).expect("Failed to read line");
 
-   
+
+    let input_int = convet_to_int(&input_str);
+    let result = 1;
+    for i in 1..input_int+1 {
+        result*=i;
+    }
+
+    println!("Factorial of {} is {}", input_int, result);
 }
